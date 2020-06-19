ae266345385a3d6f86fc462c62f098d3c12b3f78
Emily Egan <emily@operata.com>
2020-06-19 12:21:13 +1000

    Updates

---
+++ precache-manifest.94426af8343e1cec47703025aab52aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4345a575997f5486a3e1b46295e08b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e93bc4991e37b9b6a108",
+    "url": "/static/css/main.f8e0204f.chunk.css"
+  },
+  {
+    "revision": "6ff3dbebd2c9137da9a9",
+    "url": "/static/js/2.5e7a29f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5e7a29f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93bc4991e37b9b6a108",
+    "url": "/static/js/main.fc5c56b1.chunk.js"
+  },
+  {
+    "revision": "8c901b8edc33d1ca1ccf",
+    "url": "/static/js/runtime-main.fe700b02.js"
+  }
+]);
